119b85e78fbbb7d1bde5e4833d3d517f044c7797
kendallValencia <kendallvalencia22@gmail.com>
2025-08-30 01:08:53 -0600

    AVANCE

--- practica de examen/practica de examen/Practica con clase_misma.cpp
+++ practica de examen/practica de examen/Practica con clase_misma.cpp
@@ -3,11 +3,19 @@
 const int Precio = 50;
 int precio_final = 0;
 int cantidad_descuento = 0;
+const int Precio = 50;
+class Venta {
 
-
+private:
+    
+    int precio_final = 0;
+    int cantidad_descuento = 0;
+public:
+    Venta(int precio_final, int cantidad_descuento);
+};
 
 int main() {
-    cout << "hola mundo";
+    
  
     return 0;
 }
